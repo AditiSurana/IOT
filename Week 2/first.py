0d8cbbd45866e34d6a46a71d5628779ee860747a
AditiSurana <suraa423@newschool.edu>
2016-09-14 15:28:49 -0400

    final

--- Week 2/first.py
+++ Week 2/first.py
@@ -6,33 +6,36 @@ print("Hi there! Let's play some dice. Enter the following information to predic
 name = raw_input("Enter your name " )
 age = int(raw_input("Enter your age " ))
 num1 = int(raw_input("Enter a number between 1 and 10 " ))
-predictions = ['Alcohol will be your best friend today', 'A grey hair is going to show itself tomorrow', 'You might as well give up on love', '']
+predictions = ['Alcohol will be your best friend today', 'A grey hair is going to show itself tomorrow', 'You might as well give up on love', 'Get a cat and all the evil will be gone', 'Start saving for a hip replacement', 'Your soulmate found someone else today. It is time you do too', 'Your cat is looking for his previous owner', 'Love will find you. So will the IRS', 'You will meet God in the F train today. Make sure to catch it', 'Today is the day you will come out of the Matrix']
 
 
 def future():
     if num1 > 0 and num1 < 11:
-        print "hello "+name+" you are " + str(num1)
+        # print "hello "+name+" you are " + str(num1)
+        x = int((age + num1)/10)
+        print x
         for i in range (len(predictions)):
-        	if i == num1:
+        	if i == x:
         		#print predictions[i]
         		global textforemail 
         		textforemail = predictions[i]
-        		#print textforemail
+        		# print textforemail
     
     else:
         print "Please enter a number between 1 and 10."
     
 future();
 
-fromaddr = "suraa423@newschool.edu"
-toaddr = "aditisurana22@gmail.com"
+fromaddr = "email"
+toaddr = "email"
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
 msg['Subject'] = "Behold your future"
 
 # print textforemail
-body = textforemail
+
+body = "Hi "+name+"! "+textforemail
 msg.attach(MIMEText(body, 'plain'))
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
